b60e59ea6b1211bec1909abc0325f0e04c56d0d5
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-08-23 18:32:59 +0530

    scss

---
+++ scss/src/App.jsx
@@ -0,0 +1,23 @@
+ import "./app.scss"
+ 
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="container">
+      <div className="box">
+        hello
+        <button>Click</button>
+        <button>Pclick</button>
+        <p className="p1">Para tag1</p>
+        <p className="p2">Para tag2</p>
+        <p className="p3">Para tag3</p>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
